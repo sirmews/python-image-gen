b1cceb8e268c367db435c2df91ae9f238a39dc5a
Nav Rao <nav.rao@mindsethealth.com>
2023-09-18 10:46:21 +0000

    resize font if there's space

--- app/font_loader.py
+++ app/font_loader.py
@@ -11,12 +11,14 @@ FONTS_JSON_PATH = os.path.join(BASE_DIR, 'fonts.json')
 # Global cache for fonts
 FONTS_CACHE = {}
 
+
 def load_font_metadata(filename='fonts.json'):
     """
     Load font metadata from the provided JSON file.
     """
     with open(FONTS_JSON_PATH) as f:
         return json.load(f)
+
 
 def fetch_and_cache_font(font_url, font_name, font_size=36):
     """
@@ -26,22 +28,28 @@ def fetch_and_cache_font(font_url, font_name, font_size=36):
         response = requests.get(font_url)
         if response.status_code == 200:
             font_file = BytesIO(response.content)
-            font = ImageFont.truetype(font_file, font_size)  # You can change the default size here
-            FONTS_CACHE[font_name] = font
+            # You can change the default size here
+            font = ImageFont.truetype(font_file, font_size)
+            # Cache the font using font_name and font_size as the key
+            key = f"{font_name}_{font_size}"
+            FONTS_CACHE[key] = font
             return font
         else:
             print(f"Error downloading {font_name} from {font_url}")
             return None
     except Exception as e:
-        print(f"An error occurred while downloading {font_name} from {font_url}: {e}")
+        print(
+            f"An error occurred while downloading {font_name} from {font_url}: {e}")
         return None
+
 
 def get_font(font_name, font_size=36):
     """
     Get a font from the cache. If not in cache, it will try to fetch and cache it.
     """
+    key = f"{font_name}_{font_size}"
     if font_name in FONTS_CACHE:
-        return FONTS_CACHE[font_name]
+        return FONTS_CACHE[key]
     else:
         # Try fetching the font if it's not already in cache
         fonts_metadata = load_font_metadata()
@@ -51,8 +59,9 @@ def get_font(font_name, font_size=36):
                 return fetch_and_cache_font(font['url'], font_name, font_size)
         print(f"Font {font_name} not found in fonts.json")
         # trigger error and exit
-        
+
         return None
+
 
 def initialize_fonts():
     """
@@ -61,6 +70,7 @@ def initialize_fonts():
     fonts_metadata = load_font_metadata()
     for font in fonts_metadata:
         fetch_and_cache_font(font['url'], font['postscript_name'])
+
 
 if __name__ == "__main__":
     # If you run this file, it will initialize all fonts (useful for testing)
--- app/image_processing.py
+++ app/image_processing.py
@@ -2,10 +2,10 @@ from font_loader import get_font, initialize_fonts
 from PIL import ImageDraw
 
 
-def add_quote_to_image(img, quote, font, padding=10):
+def add_quote_to_image(img, quote, font_name, padding=10):
     """
     Adds a quote to the provided image, ensuring the quote doesn't exceed the image's boundaries.
-    
+
     Args:
         img (PIL.Image): The image to add the quote to.
         quote (str): The quote text to be added.
@@ -15,33 +15,74 @@ def add_quote_to_image(img, quote, font, padding=10):
     Returns:
         PIL.Image: Image with the quote added.
     """
-    font_name = font
-    font = get_font(font_name, font_size=50)
-
+    max_font_size = 100  # set a reasonable max font size
     draw = ImageDraw.Draw(img)
-    
-    # Calculate the bounding box of the text
-    text_bbox = draw.textbbox((0, 0), quote, font=font)
-    
-    # Width and height of the bounding box
-    text_width = text_bbox[2] - text_bbox[0]
-    text_height = text_bbox[3] - text_bbox[1]
-
-
-
-    # Scale down the font size if the quote doesn't fit within the image
-
-    # while text_width < img.width - (2 * padding) or text_height < img.height - (2 * padding):
-    #     font_size += 5
-    #     font = get_font(font_name, font_size)
-    #     text_bbox = draw.textbbox((0, 0), quote, font=font)
-    #     text_width = text_bbox[2] - text_bbox[0]
-    #     text_height = text_bbox[3] - text_bbox[1]
-
-    # Calculate X, Y position of the text
-    x = (img.width - text_width) / 2
-    y = (img.height - text_height) / 2
-
-    draw.text((x, y), quote, (255,255,255), font=font)
-    
+    optimal_font_size = get_optimal_font_size(
+        draw, quote, font_name, max_font_size, padding, img.width, img.height)
+    optimal_font = get_font(font_name, font_size=optimal_font_size)
+    draw_centered_text(img, quote, optimal_font)
     return img
+
+
+def calculate_text_bbox(draw, quote, font):
+    """Return the bounding box of the text for the given quote and font."""
+    return draw.textbbox((0, 0), quote, font=font)
+
+
+def draw_centered_text(img, quote, font):
+    """
+    Draw the given text centered on the image.
+
+    Args:
+    - img (PIL.Image): The image to draw the text on.
+    - quote (str): The text to be drawn.
+    - font (PIL.ImageFont.FreeTypeFont): The font object to use.
+
+    Returns:
+    - None: The function directly modifies the given image.
+    """
+    draw = ImageDraw.Draw(img)
+    text_bbox = calculate_text_bbox(draw, quote, font)
+
+    # Compute X and Y coordinates for centered placement of the text
+    x = (img.width - text_bbox[2]) / 2
+    y = (img.height - text_bbox[3]) / 2
+
+    draw.text((x, y), quote, (255, 255, 255), font=font)
+
+
+def get_optimal_font_size(draw, quote, font_name, max_size, padding, image_width, image_height):
+    """
+    Find the optimal font size for the given quote within the specified image dimensions.
+
+    Args:
+    - draw (ImageDraw.Draw): A drawing context for the image.
+    - quote (str): The text we want to draw.
+    - font_name (str): The font's name.
+    - max_size (int): Maximum permissible font size.
+    - padding (int): The padding from the image's borders.
+    - image_width (int): Width of the image.
+    - image_height (int): Height of the image.
+
+    Returns:
+    - int: The optimal font size for the quote to fit in the image.
+    """
+    base_font_size = 20  # Start with a reasonably small size to increment from
+
+    while base_font_size < max_size:
+        font = get_font(font_name, font_size=base_font_size)
+
+        # Calculate the bounding box of the text for this font size
+        text_bbox = calculate_text_bbox(draw, quote, font)
+        text_width = text_bbox[2] - text_bbox[0]
+        text_height = text_bbox[3] - text_bbox[1]
+
+        # If the quote width or height exceeds the image dimensions minus padding,
+        # return the previous font size that fit
+        if text_width > image_width - (2 * padding) or text_height > image_height - (2 * padding):
+            return base_font_size - 10
+
+        # Increment the font size for the next iteration
+        base_font_size += 5
+
+    return base_font_size
